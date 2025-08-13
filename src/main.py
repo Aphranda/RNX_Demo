c140a3f192e00c77ce65c70bb248ddd8021dd17f
Dong Li <1044770247@qq.com>
2025-08-13 17:26:20 +0800

    修改打包文件

---
+++ src/main.py
@@ -0,0 +1,47 @@
+from PyQt5.QtWidgets import (
+    QApplication
+)
+from PyQt5.QtCore import QDir
+
+
+# 1. 设置项目路径
+import numpy as np
+import pandas as pd
+import pyvisa as visa
+import scipy
+import sys
+from pathlib import Path
+
+# 将项目根目录加入系统路径
+BASE_DIR = Path(__file__).parent.parent
+sys.path.append(str(BASE_DIR))
+
+from app.core.tcp_client import TcpClient
+from app.main_window import MainWindow
+from app.utils.ProcessManager import ProcessManager
+from app.utils.SignalUnitConverter import SignalUnitConverter
+from app.controllers.CalibrationFileManager import CalibrationFileManager
+
+if __name__ == "__main__":
+    # 先检查是否已有实例运行
+    process_mgr = ProcessManager()
+    if process_mgr.check_duplicate_instance():
+        sys.exit(1)
+    
+    communicator = TcpClient()
+    unit_converter = SignalUnitConverter()
+    calibrationFileManager = CalibrationFileManager
+    
+    # 正常启动主程序
+    app = QApplication(sys.argv)
+
+    # 初始化资源系统
+    QDir.addSearchPath('resources', 'resources')  # 添加资源搜索路径
+    
+    # 设置应用程序名称（用于任务管理器识别）
+    app.setApplicationName("RNX Quantum Antenna Test System")
+    app.setApplicationDisplayName("RNX量子天线测试系统")
+    
+    window = MainWindow(communicator,unit_converter,calibrationFileManager)
+    window.show()
+    sys.exit(app.exec_())
